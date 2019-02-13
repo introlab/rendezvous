bfbb8926116b03fd06b1b46883be71426dec6137
alexmo16 <alexismoreldev@gmail.com>
2019-02-12 20:59:49 -0500

    configuration_helper started based on what was done in Auditus

---
+++ scripts/ConfigurationHelper/configuration_helper.py
@@ -0,0 +1,34 @@
+import sys
+import argparse
+
+from OdasStream.odas_stream import OdasStream
+
+
+def createParser():
+    parser = argparse.ArgumentParser(description='Helping with 16SoundUSB configurations')
+
+    parser.add_argument('--cfgPath', dest='configPath', action='store', help='Path to the config file for ODAS', required=True)
+    parser.add_argument('--odasPath', dest='odasPath', action='store', help='Path to odaslive program', required=True)
+
+    return parser
+
+
+def main():
+    try:
+        print('configuration_helper starting...')
+
+        parser = createParser()
+        args = parser.parse_args()
+
+        stream = OdasStream(args.odasPath, args.configPath)
+        stream.start()
+        sys.exit(0)
+    
+    except Exception as e:
+        print('Exception : ', e)
+        sys.exit(-1)
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/__init__.py
@@ -0,0 +1,8 @@
+import sys
+
+def main():
+    print('unit testing')
+    sys.exit(0)
+
+if __name__ == '__main__':
+    main()
