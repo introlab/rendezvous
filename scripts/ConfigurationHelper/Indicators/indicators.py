23d9bfbae04cc3b2366ff901e6f38a4929f486a7
Mathieu Favreau <Mathieu.Favreau@usherbrooke.ca>
2019-02-19 16:19:48 -0500

    Read the sources config file and calculate the area where the source should be in

--- scripts/ConfigurationHelper/Indicators/indicators.py
+++ scripts/ConfigurationHelper/Indicators/indicators.py
@@ -8,29 +8,57 @@ class Indicators:
         self.eventsPerSrc = [0, 0, 0, 0]
         self.azimuth = {'sum' : [0, 0, 0, 0], 'average' : [0, 0, 0, 0]}
         self.elevation = {'sum' : [0, 0, 0, 0], 'average' : [0, 0, 0, 0]}
+        self.configSources = []
 
 
     def indicatorsCalculation(self):
+        self.__initializeConfigSources()
         self.__sumCalculation()
         self.__averageCalculation()
 
 
+    def __initializeConfigSources(self):
+        detectionArea = self.config['DetectionArea']
+        width = detectionArea['Width']
+        height = detectionArea['Height']
+
+        for source in self.config['Sources']:
+            x = source['x']
+            y = source['y']
+            z = source['z']
+
+            # Calculate the acceptable range of the azimuth angle
+            xyHypotenuse = math.sqrt(x**2 + y**2)
+            dThetaAz = math.sin(width / (2 * xyHypotenuse))
+            azimuth = math.atan2(y, x) % (2 * math.pi)
+            azimuthBounds = { 'min' : azimuth - dThetaAz, 'max' : azimuth + dThetaAz }
+
+            # Calculate the acceptable range of the elevation angle
+            xyzHypotenuse = math.sqrt(x**2 + y**2 + z**2)
+            dThetaEl = math.sin(height / (2* xyzHypotenuse))
+            elevation = math.atan2(z, xyHypotenuse) % (2 * math.pi)
+            elevationBounds = { 'min' : elevation - dThetaEl, 'max' : elevation + dThetaEl }
+
+            self.configSources.append({'x' : x, 'y' : y, 'z' : z, 
+                'azimuthBounds' : azimuthBounds, 'elevationBounds' : elevationBounds})
+
+
     def __sumCalculation(self):
         # calculate indicators for each events
-            for event in self.events:
-                sources = event[0]['src']
+        for event in self.events:
+            sources = event[0]['src']
 
-                for key, source in sources.items():
-                    sourceId = int(key)
-                    self.eventsPerSrc[sourceId - 1] += 1
-                    x = source['x']
-                    y = source['y']
-                    z = source['z']
-                    xyHypotenuse = math.sqrt(y**2 + x**2)
-                    azimuth = (math.atan2(y, x) * 360 / 2*math.pi) % 360 
-                    elevation = (math.atan2(z, xyHypotenuse) * 360 / 2*math.pi) % 360
-                    self.azimuth['sum'][sourceId - 1] += azimuth
-                    self.elevation['sum'][sourceId - 1] += elevation
+            for key, source in sources.items():
+                sourceId = int(key)
+                self.eventsPerSrc[sourceId - 1] += 1
+                x = source['x']
+                y = source['y']
+                z = source['z']
+                xyHypotenuse = math.sqrt(y**2 + x**2)
+                azimuth = (math.atan2(y, x) * 360 / 2*math.pi) % 360 
+                elevation = (math.atan2(z, xyHypotenuse) * 360 / 2*math.pi) % 360
+                self.azimuth['sum'][sourceId - 1] += azimuth
+                self.elevation['sum'][sourceId - 1] += elevation
 
 
     def __averageCalculation(self):
