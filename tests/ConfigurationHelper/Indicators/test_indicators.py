32bb490278b2608c91e2b740b87549f88a026ef2
alexmo16 <alexismoreldev@gmail.com>
2019-02-22 15:30:23 -0500

    Rdv 100 (#2) test pipeline
    * RDV-100 : add tests struture
    
    * travis-ci file
    
    * travis.yml modified
    
    * fix travis.yml

---
+++ tests/ConfigurationHelper/Indicators/test_indicators.py
@@ -0,0 +1,23 @@
+import unittest
+import pathlib
+import os
+
+from scripts.ConfigurationHelper.Indicators.indicators import Indicators
+
+rootPath = pathlib.Path('../../../').parents[2].absolute()
+
+
+class TestIndicators(unittest.TestCase):
+
+    def setUp(self):
+        events = []
+        srccpath = os.path.join(rootPath, 'config/testconfigs/sources/config1.json')
+        if not os.path.exists(srccpath):
+            self.fail('config file for testing does not exists at : {path}'.format(path=srccpath))
+
+        self.indicators = Indicators(events, srccpath)
+
+    
+    def test_azimuthCalculation(self):
+        azimuth = self.indicators.azimuthCalculation(0, 0)
+        self.assertEqual(azimuth, 0)
