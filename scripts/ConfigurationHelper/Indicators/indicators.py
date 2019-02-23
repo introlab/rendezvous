bb3f8a06975c38ce4dba8f6720407bc0b3765ef9
alexmo16 <alexismoreldev@gmail.com>
2019-02-23 14:14:23 -0500

    oups

--- scripts/ConfigurationHelper/Indicators/indicators.py
+++ scripts/ConfigurationHelper/Indicators/indicators.py
@@ -52,14 +52,14 @@ class Indicators:
             xyHypotenuse = math.sqrt(x**2 + y**2)
             dThetaAz = math.sin(width / (2 * xyHypotenuse))
             azimuth = self.azimuthCalculation(y, x)
-            print('Reel azimuth for source {source} equals = {azimuth}'.format(source=index+1, azimuth=azimuth * 180 / math.pi))
+            print('Real azimuth for source {source} equals = {azimuth}'.format(source=index+1, azimuth=azimuth * 180 / math.pi))
             azimuthBounds = { 'min' : azimuth - dThetaAz, 'max' : azimuth + dThetaAz }
 
             # Calculate the acceptable range of the elevation angle
             xyzHypotenuse = math.sqrt(x**2 + y**2 + z**2)
             dThetaEl = math.sin(height / (2* xyzHypotenuse))
             elevation = self.elevationCalculation(z, xyHypotenuse)
-            print('Reel elevation for source {source} equals = {elevation}'.format(source=index+1, elevation=elevation * 180 / math.pi))
+            print('Real elevation for source {source} equals = {elevation}'.format(source=index+1, elevation=elevation * 180 / math.pi))
             elevationBounds = { 'min' : elevation - dThetaEl, 'max' : elevation + dThetaEl }
 
             self.configSources.append({'x' : x, 'y' : y, 'z' : z, 
