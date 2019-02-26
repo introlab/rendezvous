4007c9756dbd5bc5f666e55dc969404dd8ceeda9
Patrick Beaulieu <patrick.beaulieu3@usherbrooke.ca>
2019-02-26 12:32:43 -0500

    allo?

--- tests/ConfigurationHelper/Indicators/test_indicators.py
+++ tests/ConfigurationHelper/Indicators/test_indicators.py
@@ -4,7 +4,7 @@ import os
 import math
 
 from scripts.ConfigurationHelper.Indicators.indicators import Indicators
-from Utils.file_helper import FileHelper
+from src.Utils.file_helper import FileHelper
 
 rootPath = pathlib.Path('../../../').parents[2].absolute()
 
