9b7effbb5ea7ccee6af987257d62846da678dd10
Rouiller <felix.roy@usherbrooke.ca>
2019-03-04 14:30:20 -0500

    Tmp

--- src/tools/speechtotext/speech_to_text.py
+++ src/tools/speechtotext/speech_to_text.py
@@ -39,16 +39,37 @@ def main():
             max_alternatives=args.maxAlternatives,
             model=args.model,
             use_enhanced=args.useEnhanced,
-            enable_automatic_punctuation=args.autoPunctuation)
+            enable_automatic_punctuation=args.autoPunctuation,
+            enable_word_time_offsets=True)
 
         # Detects speech in the audio data.
-        response = client.recognize(config, audio)
+        # response = client.recognize(config, audio)
 
-        for result in response.results:
-            for i in range(len(result.alternatives)):
-                print('Transcript {alternative} : \n {result} \n'.format(
-                alternative=i+1,
-                result=result.alternatives[i].transcript))
+        # for result in response.results:
+        #     for i in range(len(result.alternatives)):
+        #         print('Transcript {alternative} : \n {result} \n'.format(
+        #         alternative=i+1,
+        #         result=result.alternatives[i].transcript))
+        
+        operation = client.long_running_recognize(config, audio)
+
+        print('Waiting for operation to complete...')
+        result = operation.result(timeout=90)
+
+        print('TEST\n')
+        for result in result.results:
+            alternative = result.alternatives[0]
+            print(u'Transcript: {}'.format(alternative.transcript))
+            print('Confidence: {}'.format(alternative.confidence))
+
+            for word_info in alternative.words:
+                word = word_info.word
+                start_time = word_info.start_time
+                end_time = word_info.end_time
+                print('Word: {}, start_time: {}, end_time: {}'.format(
+                    word,
+                    start_time.seconds + start_time.nanos * 1e-9,
+                    end_time.seconds + end_time.nanos * 1e-9))
 
     except Exception as e:
         print('Exception : ', e)
