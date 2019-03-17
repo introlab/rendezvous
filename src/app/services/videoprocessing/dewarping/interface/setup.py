dbb5e975ea745adb0c96e872c1057888c87806b8
walidm95 <walid.madaoui@gmail.com>
2019-03-17 02:35:34 -0400

    Fix dewarping compilation

--- src/app/services/videoprocessing/dewarping/interface/setup.py
+++ src/app/services/videoprocessing/dewarping/interface/setup.py
@@ -9,7 +9,7 @@ setup(name=name, version=version,
       # distutils detects .i files and compiles them automatically
       ext_modules=[Extension(name='_fisheye_dewarping', # SWIG requires _ as a prefix for the module name
                              sources=['fisheye_dewarping.i', 'src/FisheyeDewarping.cpp'],
-                             include_dirs=['src', '../include', libpath + '/glfw-3.2.1/include', libpath + '/glm/include', libpath + '/glad/include', numpy.get_include()],
+                             include_dirs=['src', '../include', libpath + '/glfw-3.2.1/include', libpath + '/glm/include', libpath + '/glad/include', libpath + '/KHR/include', numpy.get_include()],
 			           library_dirs=['../lib', libpath + '/glfw-3.2.1/build/src'],
 			           libraries=['Dewarper', 'GL', 'GLU', 'glfw3', 'X11', 'Xxf86vm', 'Xrandr', 'pthread', 'Xi', 'dl', 'Xinerama', 'Xcursor'],
                              swig_opts=['-c++'],
