809064079ab5daefd94c7f21efc99ebbe620213d
Patrick Beaulieu <patrick.beaulieu3@usherbrooke.ca>
2019-04-02 16:36:30 -0400

    yolo

--- src/app/services/videoprocessing/facedetection/facedetector/yolo_face_detector.py
+++ src/app/services/videoprocessing/facedetection/facedetector/yolo_face_detector.py
@@ -13,12 +13,12 @@ rootDirectory = str(Path(__file__).resolve().parents[6])
 class YoloFaceDetector(IFaceDetector):
 
     def __init__(self):
-        self.cfg = os.path.join(rootDirectory, "config/yolo/cfg/yolov3-tiny.cfg")
-        self.weights = os.path.join(rootDirectory, "config/yolo/weights/yolov3-tiny.weights")
-        self.data = os.path.join(rootDirectory, "config/yolo/cfg/coco.data")
+        self.cfg = os.path.join(rootDirectory, "config/yolo/cfg/tiny-yolo-azface-fddb.cfg")
+        self.weights = os.path.join(rootDirectory, "config/yolo/weights/tiny-yolo-azface-fddb_82000.weights")
+        self.data = os.path.join(rootDirectory, "config/yolo/cfg/azface.data")
         self.net = Detector(bytes(self.cfg, encoding="utf-8"), bytes(self.weights, encoding="utf-8"), 0,
                             bytes(self.data, encoding="utf-8"))
-        self.probabilityThreshold = 0.5
+        self.probabilityThreshold = 0.25
 
 
     def detectFaces(self, image):
