f9b71dce158c7b8594ae28f1fa36ef34e8f75282
alexmo16 <alexismoreldev@gmail.com>
2019-04-09 13:40:09 -0400

    Rdv 142 video recorder (#39)

--- src/app/services/odas/odas.py
+++ src/app/services/odas/odas.py
@@ -69,7 +69,7 @@ class Odas(QObject, Thread):
                             worker.start()
                             self.__workers.append(worker)
 
-                            worker = self.__initWorker(clientAudio, 1024)
+                            worker = self.__initWorker(clientAudio, 3072)
                             worker.start()
                             self.__workers.append(worker)
 
@@ -155,8 +155,8 @@ class Odas(QObject, Thread):
         if self.odasProcess:
             if self.isConnected:
                 self.closeConnections()
-            self.odasProcess.signalException.disconnect(self.odasLiveExceptionHandling)
             self.odasProcess.stop()
+            self.odasProcess.signalException.disconnect(self.odasLiveExceptionHandling)
             self.odasProcess = None
             print('odas subprocess stopped...') if self.isVerbose else None
 
---
+++ src/app/services/recorder/video/video_writer.py
@@ -0,0 +1,29 @@
+from threading import Thread
+import numpy as np
+import cv2
+
+from PyQt5.QtCore import QObject, pyqtSignal
+
+class VideoWriter(QObject):
+
+    def __init__(self, filepath, fourCC, fps, parent=None):
+        super(VideoWriter, self).__init__(parent)
+        
+        self.__filepath = filepath
+        self.fourCC = cv2.VideoWriter_fourcc(*fourCC)
+        self.fps = fps
+        self.__writer = None
+
+
+    def write(self, frame):
+        if not self.__writer:
+            height, width, _ = frame.shape
+            self.__writer = cv2.VideoWriter(self.__filepath, self.fourCC, self.fps, (width, height))
+        
+        self.__writer.write(frame)
+    
+
+    def close(self):
+        if self.__writer:
+            self.__writer.release()
+
