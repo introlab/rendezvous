f6700c7c30de45e9ef3cd8fa46bf7781973c94f4
Patrick Beaulieu <patrick.beaulieu3@usherbrooke.ca>
2019-04-14 18:02:48 -0400

    fixed

--- src/app/services/videoprocessing/virtualcamera/virtual_camera_displayer.py
+++ src/app/services/videoprocessing/virtualcamera/virtual_camera_displayer.py
@@ -42,5 +42,5 @@ class VirtualCameraDisplayer:
     def __createQImageFromOpenCVImage(self, image):
         imageHeight, imageWidth, colors = image.shape
         bytesPerLine = 3 * imageWidth
-        qImage = QImage(image.data, imageWidth, imageHeight, bytesPerLine, QImage.Format.RGB888).rgbSwapped()
+        qImage = QImage(image.data, imageWidth, imageHeight, bytesPerLine, QImage.Format_RGB888).rgbSwapped()
         return qImage
