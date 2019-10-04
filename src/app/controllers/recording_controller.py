fa801bf6624ca5e0015ff5cc1c66c08b6c77c65b
alexmo16 <alexismoreldev@gmail.com>
2019-10-04 17:45:10 -0400

    maybe

--- src/app/controllers/recording_controller.py
+++ src/app/controllers/recording_controller.py
@@ -148,7 +148,7 @@ class RecordingController(QObject):
 
     @pyqtSlot(object, object)
     def __virtualCamerasReceived(self, images, virtualCameras):
-        if self.__recorder and self.__recorder.state == ServiceState.RUNNING:
+        if self.__recorder and self.__recorder.state == ServiceState.RUNNING and self.__videoProcessor.state == ServiceState.RUNNING:
             combinedImage = VirtualCameraDisplayBuilder.buildImage(images, (800, 600),
                                                         self.__virtualCameraFrame.palette().color(QtGui.QPalette.Background), 10) 
                                                         
