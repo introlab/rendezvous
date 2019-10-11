74663ac1f853f39d4273bd5572619df704c6963b
walidm95 <walid.madaoui@gmail.com>
2019-10-11 16:34:35 -0400

    test file that shows a green screen in the virtual cam device

---
+++ src/app/services/virtualcameradevice/test.py
@@ -0,0 +1,15 @@
+import time
+from virtual_camera_device import VirtualCameraDevice
+
+def main():
+    virtualCameraDevice = VirtualCameraDevice(videoDevice="/dev/video1", format=0, width=640, height=480, fps=15)
+
+    virtualCameraDevice.test()
+
+    time.sleep(5)
+
+    virtualCameraDevice.__del__
+
+
+if __name__ == '__main__':
+	main()
