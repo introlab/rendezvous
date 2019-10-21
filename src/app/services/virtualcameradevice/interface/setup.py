3f9686e43f0a4b34534d823f8d4d2455c843efd3
alexmo16 <alexismoreldev@gmail.com>
2019-10-21 11:38:20 -0400

    fix-travis

--- src/app/services/virtualcameradevice/interface/setup.py
+++ src/app/services/virtualcameradevice/interface/setup.py
@@ -9,7 +9,7 @@ setup(name=name, version=version,
       ext_modules=[Extension(name='_virtual_camera_device', # SWIG requires _ as a prefix for the module name
                              sources=['virtual_camera_device.i', 'src/VirtualCameraDevice.cpp'],
                              include_dirs=['src', '../include', numpy.get_include()],
-                             library_dirs=['../../../../../../../lib/libv4l2cpp'],
+                             library_dirs=['../../../../../lib/libv4l2cpp'],
                              libraries=['v4l2wrapper'],
                              swig_opts=['-c++'],
                              extra_compile_args=['-std=c++14'])
