b8feab30dae8a84421be80d7c5f30c2717e5a08e
walidm95 <walid.madaoui@gmail.com>
2019-10-21 16:55:51 -0400

    Implement UI of the recording view

--- c_plus_plus/src/view/views/recording_view.cpp
+++ c_plus_plus/src/view/views/recording_view.cpp
@@ -6,9 +6,65 @@ namespace View
 
 RecordingView::RecordingView(QWidget *parent)
     : AbstractView("Recording", parent)
-    , ui(new Ui::RecordingView)
+    , mUi(new Ui::RecordingView)
 {
-    ui->setupUi(this);
+    mUi->setupUi(this);
+
+    mCameraViewfinder = new QCameraViewfinder(this);
+    mCameraViewfinder->show();
+
+    mCamera = new QCamera(getCameraInfo());
+    mCamera->setViewfinder(mCameraViewfinder);
+
+    mUi->virtualCameraLayout->addWidget(mCameraViewfinder);
+
+    connect(mUi->btnStartStopRecord, &QAbstractButton::clicked, [=]{ changeRecordButtonState(); });
+}
+
+QCameraInfo RecordingView::getCameraInfo()
+{
+    QCameraInfo cameraInfo;
+    QList<QCameraInfo> cameras = QCameraInfo::availableCameras();
+    if(cameras.length() == 1)
+    {
+        cameraInfo = QCameraInfo::defaultCamera();
+    }
+    else
+    {
+        foreach (const QCameraInfo &camInfo, cameras)
+        {
+            if (camInfo.deviceName() == "/dev/video1")   //TODO: get device from config file
+                cameraInfo = camInfo;
+        }
+    }
+
+    return cameraInfo;
+}
+
+void RecordingView::changeRecordButtonState()
+{
+    mRecordButtonState = !mRecordButtonState;
+
+    if(mRecordButtonState)
+        mUi->btnStartStopRecord->setText("Stop recording");    // TODO: Call start on IRecorder
+    else
+        mUi->btnStartStopRecord->setText("Start recording");   // TODO: Call stop on IRecorder
+}
+
+void RecordingView::showEvent(QShowEvent *event)
+{
+    if(mCamera->state() != QCamera::State::ActiveState)
+    {
+        mCamera->start();
+    }
+}
+
+void RecordingView::hideEvent(QHideEvent *event)
+{
+    if(mCamera->state() == QCamera::State::ActiveState)
+    {
+        mCamera->stop();
+    }
 }
 
 } // View
--- c_plus_plus/src/view/views/recording_view.h
+++ c_plus_plus/src/view/views/recording_view.h
@@ -1,6 +1,11 @@
 #ifndef RECORDING_VIEW_H
 #define RECORDING_VIEW_H
 
+#include <QCamera>
+#include <QCameraInfo>
+#include <QCameraViewfinder>
+#include <QListWidgetItem>
+
 #include "view/views/abstract_view.h"
 
 namespace Ui { class RecordingView; }
@@ -13,8 +18,18 @@ class RecordingView : public AbstractView
 public:
     explicit RecordingView(QWidget *parent = nullptr);
 
+protected:
+    void showEvent(QShowEvent *event) override;
+    void hideEvent(QHideEvent *event) override;
+
 private:
-    Ui::RecordingView *ui;
+    QCameraInfo getCameraInfo();
+    void changeRecordButtonState();
+
+    Ui::RecordingView *mUi;
+    QCamera *mCamera;
+    QCameraViewfinder *mCameraViewfinder;
+    bool mRecordButtonState = false;
 };
 
 } // View
