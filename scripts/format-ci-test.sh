87f5f13bbbda1b7f474943cbb8960f55cf1ae564
alexmo16 <alexismoreldev@gmail.com>
2019-10-26 14:18:44 -0400

    test formatting failed on ci

---
+++ scripts/format-ci-test.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+find ./c_plus_plus/ -regex '.*\.\(cpp\|hpp\|cu\|c\|h\)' -exec clang-format -style=file -i {} \; 
+
+failedMsg="C++ formatting test failed because of these files:"
+dirty=$(git ls-files --modified)
+
+if [[ $dirty ]]; then
+    echo $failedMsg
+    echo $dirty
+    exit 1
+fi
+
+echo "C++ formatting test passed!!"
+exit 0
