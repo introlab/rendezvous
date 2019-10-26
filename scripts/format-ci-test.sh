00d9994086869271f288895a6024ee96269ad269
alexmo16 <alexismoreldev@gmail.com>
2019-10-26 19:47:38 -0400

    changes

--- scripts/format-ci-test.sh
+++ scripts/format-ci-test.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-clean=$(git ls-files --modified)
+clean="$(git ls-files --modified)"
+echo $clean
+
 chmod +x ./scripts/format.sh
 ./scripts/format.sh  
 
-dirty=$(git ls-files --modified)
+dirty="$(git ls-files --modified)"
 failedMsg="C++ formatting test failed, please format your code:"
 
-if [[ "$dirty" == "$clean" ]]; then
+if [[ $dirty == $clean ]]; then
     echo "C++ formatting test passed!!"
     exit 0
 fi
