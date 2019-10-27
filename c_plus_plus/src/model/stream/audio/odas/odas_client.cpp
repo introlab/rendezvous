63f63a7e10db110f59b013b2d48ce0993cd45660
alexmo16 <alexismoreldev@gmail.com>
2019-10-27 15:24:18 -0400

    process class member removed

--- c_plus_plus/src/model/stream/audio/odas/odas_client.cpp
+++ c_plus_plus/src/model/stream/audio/odas/odas_client.cpp
@@ -7,32 +7,25 @@
 
 namespace Model
 {
-OdasClient::OdasClient()
-    : m_pProcess(std::make_unique<QProcess>())
-{
-    m_pProcess->moveToThread(this);
-    qInfo() << Model::ODAS_LIBRARY;
-    qInfo() << Model::MICROPHONE_CONFIGURATION;
-}
-
 void OdasClient::run()
 {
     m_isRunning = true;
 
-    QString program = Model::ODAS_LIBRARY;
+    const QString& program = Model::ODAS_LIBRARY;
     QStringList arguments;
     arguments << "-c" << Model::MICROPHONE_CONFIGURATION;
-    m_pProcess->start(program, arguments);
+    QProcess process;
+    process.start(program, arguments);
 
     bool returnValue = false;
     QByteArray output;
     while (m_isRunning)
     {
-        returnValue = m_pProcess->waitForFinished(1000);
+        returnValue = process.waitForFinished(1000);
         if (returnValue)
         {
             qCritical() << "Odaslive stopped working.";
-            output.append(m_pProcess->readAll());
+            output.append(process.readAll());
             if (!output.isEmpty())
             {
                 qWarning() << "Odas output:" << output;
@@ -41,12 +34,12 @@ void OdasClient::run()
         }
     }
 
-    m_pProcess->close();
-    m_pProcess->waitForFinished(500);
-    if (m_pProcess->isOpen())
+    process.close();
+    process.waitForFinished(500);
+    if (process.isOpen())
     {
         qWarning() << "Odaslive were killed.";
-        m_pProcess->kill();
+        process.kill();
     }
 }
 }
--- c_plus_plus/src/model/stream/audio/odas/odas_client.h
+++ c_plus_plus/src/model/stream/audio/odas/odas_client.h
@@ -1,7 +1,6 @@
 #ifndef ODAS_CLIENT_H
 #define ODAS_CLIENT_H
 
-#include <QProcess>
 #include <QThread>
 
 namespace Model
@@ -11,12 +10,9 @@ class OdasClient : public QThread
     Q_OBJECT
 
    public:
-    OdasClient();
     void stop() { m_isRunning = false; }
    private:
     void run() override;
-
-    std::unique_ptr<QProcess> m_pProcess;
     bool m_isRunning = false;
 };
 }
