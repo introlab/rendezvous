917797cf2b61b31d1b701691ed5cc2e582312940
alexmo16 <alexismoreldev@gmail.com>
2019-10-27 15:27:16 -0400

    put real main.pp

--- c_plus_plus/src/main.cpp
+++ c_plus_plus/src/main.cpp
@@ -1,49 +1,23 @@
-#include <QCoreApplication>
-#include <QDebug>
-#include <QThread>
-#include <memory>
-#include "model/audio/i_audio_source.h"
-#include "model/audio/i_position_source.h"
-#include "model/audio/odas/odas_audio_source.h"
-#include "model/audio/odas/odas_client.h"
-#include "model/audio/odas/odas_position_source.h"
-#include "model/audio/source_position.h"
+#include "model/media_player/media_player.h"
+#include "model/settings/settings.h"
 #include "view/mainwindow.h"
-#define AUDIO_BUFFER_SIZE 4096
 
-int main(int argc, char* argv[])
+#include <QApplication>
+#include <QFile>
+
+int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    QApplication a(argc, argv);
 
-    QThread* thread = QThread::create([&] {
-        std::unique_ptr<Model::IPositionSource> odasPositionSource = std::make_unique<Model::OdasPositionSource>(10020);
-        if (!odasPositionSource->open()) exit(-1);
-        std::unique_ptr<Model::IAudioSource> odasAudioSource = std::make_unique<Model::OdasAudioSource>(10030);
-        if (!odasAudioSource->open()) exit(-1);
-        FILE* m_file = fopen("allo.raw", "ab");
-        uint8_t audioBuffer[ AUDIO_BUFFER_SIZE ];
+    QFile file(":/stylesheets/globalStylesheet.qss");
+    file.open(QFile::ReadOnly);
+    a.setStyleSheet(QLatin1String(file.readAll()));
 
-        std::unique_ptr<Model::OdasClient> odasClient = std::make_unique<Model::OdasClient>();
-        odasClient->start();
+    Model::Settings settings;
+    Model::MediaPlayer mediaPlayer;
 
-        while (true)
-        {
-            QCoreApplication::processEvents();
-            int audioBytesRead = odasAudioSource->read(audioBuffer, AUDIO_BUFFER_SIZE);
-            if (audioBytesRead > 0)
-            {
-                fwrite(audioBuffer, sizeof(audioBuffer[ 0 ]), audioBytesRead, m_file);
-            }
-            std::vector<Model::SourcePosition> positions = odasPositionSource->getPositions();
-            for (auto pos : positions)
-            {
-                qDebug() << "azimuth = " << pos.azimuth << ", elevation = " << pos.elevation;
-            }
-            QThread::usleep(100);
-        }
-        fclose(m_file);
-        odasClient->stop();
-    });
-    thread->start();
-    return a.exec();
+    View::MainWindow w(settings, mediaPlayer);
+    w.show();
+
+    return QApplication::exec();
 }
