21128a04501bca9480ffcecba858c1d49c856c0c
Patrick Beaulieu <patrick.beaulieu3@usherbrooke.ca>
2019-11-02 15:41:08 -0400

    Merge branch 'master' into RDV-342-odassource

--- src/app/services/videoprocessing/dewarping/src/models/RawModel.cpp
+++
@@ -1,7 +0,0 @@
-#include <models/RawModel.h>
-
-RawModel::RawModel(GLuint vaoId, int vertexCount)
-    : vaoId(vaoId),
-    vertexCount(vertexCount)
-{
-}
---
+++ steno/src/model/stream/audio/audio_config.h
@@ -0,0 +1,35 @@
+#ifndef AUDIO_CONFIG_H
+#define AUDIO_CONFIG_H
+
+#include <string>
+
+#include "model/stream/utils/images/image_format.h"
+#include "model/stream/utils/models/dim2.h"
+
+namespace Model
+{
+struct AudioConfig
+{
+    AudioConfig(const std::string& deviceName, int channels, int rate, int formatBytes, bool isLittleEndian, int packetAudioSize, int packetHeaderSize)
+        : deviceName(std::move(deviceName))
+        , channels(channels)
+        , rate(rate)
+        , formatBytes(formatBytes)
+        , isLittleEndian(isLittleEndian)
+        , packetAudioSize(packetAudioSize)
+        , packetHeaderSize(packetHeaderSize)
+    {
+    }
+
+    std::string deviceName;
+    int channels;
+    int rate;
+    int formatBytes;
+    bool isLittleEndian;
+    int packetAudioSize;
+    int packetHeaderSize;
+};
+
+}    // namespace Model
+
+#endif    // !AUDIO_CONFIG_H
