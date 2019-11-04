940854e944484801702551ccc4e1196eb7dc4516
alexmo16 <alexismoreldev@gmail.com>
2019-11-03 19:25:27 -0500

    GStorage class

--- transcription_api/src/controllers/users.js
+++
@@ -1,9 +0,0 @@
-let express = require('express');
-let router = express.Router();
-
-/* GET users listing. */
-router.get('/users', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-module.exports = router;
