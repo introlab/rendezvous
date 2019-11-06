5876b170c03f9f1b08b932e5edfe056d0c5e6bd8
alexmo16 <alexismoreldev@gmail.com>
2019-11-06 13:09:52 -0500

    fix v4l2loopback automation script

--- scripts/StenoCam/automation.sh
+++ scripts/StenoCam/automation.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sudo chmod +x ~/dev/workspace/rendezvous/scripts/StenoCam/stenocam-creation.sh
-sudo cp ~/dev/workspace/rendezvous/scripts/StenoCam/stenocam-creation.sh /usr/bin/stenocam-creation.sh
+sudo chmod +x $PWD/scripts/StenoCam/stenocam-creation.sh
+sudo cp $PWD/scripts/StenoCam/stenocam-creation.sh /usr/bin/stenocam-creation.sh
 sudo chmod +x /usr/bin/stenocam-creation.sh
 
-sudo cp ~/dev/workspace/rendezvous/scripts/StenoCam/stenocam.service /etc/systemd/system/stenocam.service
+sudo cp $PWD/scripts/StenoCam/stenocam.service /etc/systemd/system/stenocam.service
 sudo chmod 644 /etc/systemd/system/stenocam.service
 
 sudo systemctl daemon-reload
