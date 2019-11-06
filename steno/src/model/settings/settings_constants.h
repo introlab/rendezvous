9a5fb6651a764a16d5da8d443389a74883236eb9
Rouiller <felix.roy@usherbrooke.ca>
2019-11-06 11:58:12 -0500

    Update of scripts and git ignore

--- steno/src/model/settings/settings_constants.h
+++ steno/src/model/settings/settings_constants.h
@@ -1,16 +1,14 @@
 #ifndef KEYS_H
 #define KEYS_H
 
+#include <QCoreApplication>
 #include <QDir>
 #include <QString>
 
 namespace Model
 {
-const QString CAMERA_CONFIGURATION = QDir::homePath();
-const QString MICROPHONE_CONFIGURATION =
-    QDir::homePath() + "/dev/workspace/rendezvous/steno/configs/odas/odas_16_mic.cfg";
-const QString ODAS_LIBRARY = QDir::homePath() + "/dev/lib/odas/bin/odaslive";
-const QString GOOGLE_SERVICE_ACCOUNT_FILE = QDir::homePath();
+const QString MICROPHONE_CONFIGURATION = QCoreApplication::applicationDirPath() + "/../configs/odas/odas_16_mic.cfg";
+const QString ODAS_LIBRARY = QCoreApplication::applicationDirPath() + "/../../odas/bin/odaslive";
 const QString VIRTUAL_CAMERA_DEVICE = "/dev/video1";
 
 namespace General
