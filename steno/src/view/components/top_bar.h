123531bbece2d02df35cfe02ffadf0aecced1084
Félix Roy <33139437+Rouiller@users.noreply.github.com>
2019-11-13 15:10:24 -0500

    Rdv 374 camera preview (#130)
    * Fix crash when application terminate
    
    * Combination of online and local view
    
    * Formatting
    
    * Refactor of Recording, display of output video device in ConferenceView
    
    * Formatting
    
    * Cleanup

--- steno/src/view/components/top_bar.h
+++ steno/src/view/components/top_bar.h
@@ -1,7 +1,7 @@
 #ifndef TOPBAR_H
 #define TOPBAR_H
 
-#include "model/recorder/i_recorder.h"
+#include "model/media/media.h"
 #include "model/stream/i_stream.h"
 
 #include <memory>
@@ -20,19 +20,18 @@ class TopBar : public QWidget
     Q_OBJECT
 
    public:
-    TopBar(std::shared_ptr<Model::IStream> stream, std::shared_ptr<Model::IRecorder> recorder,
-           QWidget* parent = nullptr);
+    TopBar(std::shared_ptr<Model::IStream> stream, std::shared_ptr<Model::Media> media, QWidget* parent = nullptr);
 
    private slots:
     void onStreamStateChanged(const Model::IStream::State& state);
     void onStartButtonClicked();
-    void onRecorderStateChanged(const Model::IRecorder::State& state);
+    void onRecorderStateChanged(const QMediaRecorder::State& state);
     void onRecordButtonClicked();
 
    private:
     Ui::TopBar* m_ui;
     std::shared_ptr<Model::IStream> m_stream;
-    std::shared_ptr<Model::IRecorder> m_recorder;
+    std::shared_ptr<Model::Media> m_media;
 };
 
 }    // namespace View
--- steno/src/view/views/conference_view.cpp
+++ steno/src/view/views/conference_view.cpp
@@ -3,15 +3,12 @@
 
 namespace View
 {
-ConferenceView::ConferenceView(std::shared_ptr<Model::IRecorder> recorder, QWidget* parent)
+ConferenceView::ConferenceView(std::shared_ptr<Model::Media> media, QWidget* parent)
     : AbstractView("Conference", parent)
     , m_ui(new Ui::ConferenceView)
-    , m_cameraViewfinder(new QCameraViewfinder(this))
 {
     m_ui->setupUi(this);
-    m_ui->layout->addWidget(m_cameraViewfinder.get());
-    m_cameraViewfinder->show();
-    recorder->setCameraViewFinder(m_cameraViewfinder);
+    media->setViewFinder(m_ui->cameraViewFinder);
 }
 
 }    // namespace View
