b3373737a07dd8419fd0c44b824814bc822cdda8
alexmo16 <alexismoreldev@gmail.com>
2019-11-26 13:39:33 -0500

    change service

--- scripts/StenoMics/automation.sh
+++ scripts/StenoMics/automation.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-sudo chmod +x $PWD/scripts/StenoMics/stenomics-creation.sh
-sudo cp $PWD/scripts/StenoMics/stenomics-creation.sh /usr/bin/stenomics-creation.sh
-sudo chmod +x /usr/bin/stenomics-creation.sh
+chmod +x $PWD/scripts/StenoMics/stenomics-creation.sh
+cp $PWD/scripts/StenoMics/stenomics-creation.sh /usr/bin/stenomics-creation.sh
+chmod +x /usr/bin/stenomics-creation.sh
 
-sudo cp $PWD/scripts/StenoMics/stenomics.service /etc/systemd/user/stenomics.service
-sudo chmod 644 /etc/systemd/user/stenomics.service
+cp $PWD/scripts/StenoMics/stenomics.service /etc/systemd/user/stenomics.service
+chmod 644 /etc/systemd/user/stenomics.service
 
 systemctl --user daemon-reload
 systemctl --user start stenomics.service && sudo systemctl --user enable stenomics.service
