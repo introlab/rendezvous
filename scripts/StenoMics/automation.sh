5eedd84c5a88f6a2be216fd68fbfa88a7b0d501f
Alexis Morel <alexismoreldev@gmail.com>
2019-11-26 14:19:37 -0500

    Rdv 346 mc automation (#148)

---
+++ scripts/StenoMics/automation.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+chmod +x $PWD/scripts/StenoMics/stenomics-creation.sh
+sudo cp $PWD/scripts/StenoMics/stenomics-creation.sh /usr/bin/stenomics-creation.sh
+sudo chmod +x /usr/bin/stenomics-creation.sh
+
+sudo cp $PWD/scripts/StenoMics/stenomics.service /etc/systemd/user/stenomics.service
+sudo chmod 644 /etc/systemd/user/stenomics.service
+
+systemctl --user daemon-reload
+systemctl --user start stenomics.service && systemctl --user enable stenomics.service
+systemctl --user daemon-reload 
