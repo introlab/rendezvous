ce4e6085147ee28bb157bf00c1f4ea876e9894bc
alexmo16 <alexismoreldev@gmail.com>
2019-11-26 13:32:49 -0500

    user

--- scripts/StenoMics/automation.sh
+++ scripts/StenoMics/automation.sh
@@ -4,8 +4,8 @@ sudo chmod +x $PWD/scripts/StenoMics/stenomics-creation.sh
 sudo cp $PWD/scripts/StenoMics/stenomics-creation.sh /usr/bin/stenomics-creation.sh
 sudo chmod +x /usr/bin/stenomics-creation.sh
 
-sudo cp $PWD/scripts/StenoMics/stenomics.service /etc/systemd/system/stenomics.service
-sudo chmod 644 /etc/systemd/system/stenomics.service
+sudo cp $PWD/scripts/StenoMics/stenomics.service /etc/systemd/user/stenomics.service
+sudo chmod 644 /etc/systemd/user/stenomics.service
 
 systemctl --user daemon-reload
 systemctl --user start stenomics.service && sudo systemctl --user enable stenomics.service
